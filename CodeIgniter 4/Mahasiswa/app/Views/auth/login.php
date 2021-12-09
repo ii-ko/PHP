c36725b0729fbe3a634352245847c477d598f301
ii-ko <mhdihsan90@gmail.com>
2021-12-09 19:10:42 +0700

    update ci4/mahasiswa

--- CodeIgniter 4/Mahasiswa/app/Views/auth/login.php
+++ CodeIgniter 4/Mahasiswa/app/Views/auth/login.php
@@ -1,23 +1,33 @@
 <?= $this->extend('layout/layout')?>
 <?= $this->section('content')?>
 <div class="row col-lg-5 mx-auto">
-    <div class="col-lg">
+  <div class="col-lg">
     <div class="card">
-        <div class="card-header"><h4 class="text-center">Welcome</h4></div>
-        <div class="card-body">
-            <form action="" method="post">
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" name="email" placeholder="Enter email address">
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" name="password" placeholder="Enter password">
-                </div>
-                <button type="submit" class="btn btn-primary">Login</button>
-            </form>
+      <div class="card-header"><h4 class="text-center">Welcome</h4></div>
+      <div class="card-body">
+        <form action="" method="post">
+          <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control" name="email" placeholder="Enter email address" />
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" name="password" placeholder="Enter password" />
+          </div>
+          <div class="d-grid gap-2">
+            <button class="btn btn-primary" type="submit">Login</button>
+          </div>
+        </form>
+        <hr />
+        <div class="form-group mb-3">
+          <p>
+            Need an account?
+            <a href="<?= base_url('/register')?>" class="text-decoration-none ml-2"> Register here! </a>
+          </p>
+          <a href="" class="text-decoration-none">Forgot Password</a>
         </div>
+      </div>
     </div>
-    </div>
+  </div>
 </div>
 <?= $this->endSection()?>
--- CodeIgniter 4/Mahasiswa/app/Views/auth/register.php
+++ CodeIgniter 4/Mahasiswa/app/Views/auth/register.php
@@ -2,30 +2,42 @@
 <?= $this->section('content')?>
 <div class="row col-lg-5 mx-auto">
     <div class="col-lg">
-    <div class="card">
-        <div class="card-header"><h4 class="text-center">Welcome</h4></div>
-        <div class="card-body">
-            <form action="" method="post">
-                <div class="mb-3">
-                    <label for="email" class="form-label">Username</label>
-                    <input type="email" class="form-control" name="email" placeholder="Enter email address">
+        <div class="card">
+            <div class="card-header"><h4 class="text-center">Create New Account</h4></div>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="form-group mb-3">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" class="form-control" name="username" placeholder="Enter Username">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" name="email" placeholder="Enter email address">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" name="password" placeholder="Enter password">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="conf_password" class="form-label">Confirm Password</label>
+                            <input type="password" class="form-control" name="conf_password" placeholder="Confirm password">
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button class="btn btn-primary" type="submit">Register</button>
+                        </div>
+                    </form>
+                    <hr>
+                    <div class="form-group mb-3">
+                        <p>Already have an account?
+                            <a href="<?= base_url('/login')?>" class="text-decoration-none ml-2">
+                                Login here!
+                            </a>
+                        </p>
+                        <a href="" class="text-decoration-none">Forgot Password</a>
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" name="email" placeholder="Enter email address">
-                </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" name="password" placeholder="Enter password">
-                </div>
-                <div class="mb-3">
-                    <label for="conf_password" class="form-label">Confirm Password</label>
-                    <input type="password" class="form-control" name="conf_password" placeholder="Enter password">
-                </div>
-                <button type="submit" class="btn btn-primary">Register</button>
-            </form>
+            </div>
         </div>
-    </div>
     </div>
 </div>
 <?= $this->endSection()?>
